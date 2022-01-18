ab4fe5e9b66d8637b4289c00dc21a520093c5f05
CarlosMontesdeoca <mateo.game@hotmail.com>
2022-01-17 20:20:46 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'boton-de-banico',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
